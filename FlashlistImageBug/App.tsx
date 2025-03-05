47100ced24db27af81a2d786669617855dca30c3
Kevin <kevin@kevincoelho.com>
2025-03-05 00:52:40 -0800

    - bug is replicated

--- FlashlistImageBug/App.tsx
+++ FlashlistImageBug/App.tsx
@@ -1,131 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+// library deps
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  ScrollView,
-  StatusBar,
+  SafeAreaView,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Image,
 } from 'react-native';
+import {FlashList} from '@shopify/flash-list';
+import {FasterImageView} from '@candlefinance/faster-image';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App(): React.JSX.Element {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <FlashList
+        ListHeaderComponent={HeaderComponent}
+        renderItem={() => <ListItem />}
+        data={new Array(10).fill(null)}
+        estimatedItemSize={10}
+      />
+    </SafeAreaView>
+  );
+}
+
+function HeaderComponent() {
+  return (
+    <View style={styles.header}>
+      <ImageComponent />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
+function ListItem() {
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <View style={styles.listItem}>
+      <NewImageComponent />
     </View>
   );
+}
+
+function ImageComponent() {
+  return (
+    <Image
+      source={{uri: 'https://picsum.photos/200/200?random=0'}}
+      resizeMode={'contain'}
+      width={100}
+      height={100}
+      borderRadius={50}
+    />
+  );
+}
+
+function NewImageComponent() {
+  return <FasterImageView
+    onError={event => {
+      console.warn(event.nativeEvent.error);
+    }}
+    style={{
+      width: 100,
+      height: 100,
+    }}
+    onSuccess={event => {
+      console.log(event.nativeEvent);
+    }}
+    source={{
+      borderRadius: 50,
+      transitionDuration: 0.3,
+      cachePolicy: 'discWithCacheControl',
+      showActivityIndicator: true,
+      url: 'https://picsum.photos/200/200?random=0',
+      grayscale: 0,
+    }}
+  />
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  safeAreaContainer: {
+    display: 'flex',
+    height: '100%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    display: 'flex',
+    height: 400,
+    backgroundColor: '#FAB',
+    paddingTop: 15,
+    paddingLeft: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  listItem: {
+    display: 'flex',
+    height: 400,
+    backgroundColor: '#DEA',
+    paddingLeft: 10,
   },
 });
-
-export default App;
